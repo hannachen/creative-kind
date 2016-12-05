689262445282844adc64f7b0b5c1fdf5a283a6f6
Hanna Chen <hanna@rethinkcanada.com>
2016-12-05 16:47:49 -0500

    Remove error log

--- app/controllers/quilts.js
+++ app/controllers/quilts.js
@@ -237,10 +237,6 @@ router.post('/create', isAuthenticated, function (req, res, next) {
               var newInvite = new Invite(inviteData);
               newInvite.save(function (err, invite) {
                 console.log('SAVED**', invite);
-                if (err) {
-                  console.log('ERROR');
-                  throw err;
-                }
               });
             });
         });
