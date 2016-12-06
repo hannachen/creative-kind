9f29e24335aa9a990aa0c54775ab0831a3f5e6b9
Hanna Chen <hanna@rethinkcanada.com>
2016-12-05 21:13:55 -0500

    push to execute post-receive

--- config/config.js
+++ config/config.js
@@ -48,7 +48,7 @@ var config = {
       service: 'Mailgun',
       auth: {
         api_key: 'key-ad0790abff28a38aab14bda682df47a1',
-        domain: 'https://api.mailgun.net/v3/creative-kind.fpo.website'
+        domain: 'api.mailgun.net/v3/creative-kind.fpo.website'
       }
     }
   },
@@ -66,7 +66,7 @@ var config = {
       service: 'Mailgun',
       auth: {
         api_key: 'key-ad0790abff28a38aab14bda682df47a1',
-        domain: 'api.mailgun.net/v3/creative-kind.fpo.website'
+        domain: 'https://api.mailgun.net/v3/creative-kind.fpo.website'
       }
     }
   },
